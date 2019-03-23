25c09740672ba3a7c3f5f2a3b0f1ce11d7b43f50
Marshall Asch <maasch@rogers.com>
2019-03-23 11:25:23 -0400

    added the auth route there are no tests still

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import {Account} from '../models/account.model';
 
+const jwt  = require('jsonwebtoken');
+
 const bcrypt = require("bcrypt");
 
+const SCECRET = "44a0a45f31cf8122651e28710a43530e";
 
 
 export class Api {
@@ -71,6 +74,49 @@ export class Api {
 
         }));
 
+
+        router.post("/auth/authenticate", ((req, res) => {
+
+
+            // check for missing params
+
+            if (!req.body.email) {
+                res.status(400).json({"message": "missing email"});
+            }
+
+            if (!req.body.password) {
+                res.status(400).json({"message": "missing password"});
+            }
+
+            new Promise((resolve, reject) => {
+                Account.findOne({
+                    email: req.body.email,
+                }).then((foundUser) => {
+
+                    if (foundUser) {
+
+                        bcrypt.compare(req.body.password, foundUser.passwordHash, function(err, passMatch) {
+
+                            if (passMatch) {
+                                var payload = {
+                                    email: foundUser.email,
+                                    fullName: foundUser.fullName,
+                                };
+
+                                var token = jwt.sign(payload, SCECRET);
+
+                                return res.status(200).json({"messaacccessTokenge": token});
+                            } else {
+                                return res.status(403).json({"message": "access denied"});
+                            }
+                        });
+                    } else {
+                        return res.status(403).json({"message": "access denied"});
+                    }
+                });
+
+            });
+        }));
 
         return router;
     }
